178b13529b26fd9d2045dff4bd33b0efca8b4900
Scott Numamoto <scott.numamoto@gmail.com>
2017-03-19 01:21:46 -0700

    improve readability

--- scrape.py
+++ scrape.py
@@ -4,10 +4,11 @@ import datetime
 from bs4 import BeautifulSoup
 
 TEST_STUDENT_WORK_SESSIONS_API = "http://localhost:3000/tomorrow_status"
-STUDENT_WORK_SESSIONS_API = "http://localhost:3000/tomorrow_status"
+STUDENT_WORK_SESSIONS_API = "http://worksessions.pierobotics.org/tomorrow_status"
 
 
 def parse_string(string):
+    "Translate a session string into a Datetime object and its attendance boolean."""
     time_format = "%m/%d/%Y, %I:%M %p"
     occupied = string[32:] == 'true'
 
@@ -15,24 +16,30 @@ def parse_string(string):
 
 
 def active_shifts():
-    #try:
-    #u = urllib.request.urlopen(STUDENT_WORK_SESSIONS_API, data=None)
-    #except urllib.error.URLError:
-    #        u = urllib.request.urlopen(TEST_STUDENT_WORK_SESSIONS_API, data=None)
-    #f = io.TextIOWrapper(u, encoding='utf-8')
-    #text = f.read()
-    soup = BeautifulSoup(open('WorksessionWebsite.htm'), 'html.parser')
+    """Return a list of datetime objects when active sessions will begin."""
 
-    result = list()
+    jank = True
+    if jank:
+        soup = BeautifulSoup(open('WorksessionWebsite.htm'), 'html.parser')
+    else:
+        try:
+            u = urllib.request.urlopen(STUDENT_WORK_SESSIONS_API, data=None)
+        except urllib.error.URLError:
+            u = urllib.request.urlopen(TEST_STUDENT_WORK_SESSIONS_API, data=None)
+        f = io.TextIOWrapper(u, encoding='utf-8')
+        text = f.read()
+        soup = BeautifulSoup(text, 'html.parser')
 
-    for shift in soup.body.find_all('div'):
-        string = shift.contents[0]
-        key, answer = parse_string(string)
+    attended_datetimes = list()
+
+    for time_slot in soup.body.find_all('div'):
+        string = time_slot.contents[0]
+        datetime, answer = parse_string(string)
         if not answer:
-            result.append(key)
-    return result
+            attended_datetimes.append(datetime)
+    return attended_datetimes
 
 if __name__ == "__main__":
-    thing = active_shifts()
-    for x in thing:
-        print(x)
+    shift_list = active_shifts()
+    for shift in shift_list:
+        print(shift)
