5b192d8560347ebba9e3b227b4df5ff94f120f39
Scott Numamoto <scott.numamoto@gmail.com>
2017-03-19 01:14:50 -0700

    Add more readability

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ from spreadsheet import sheets
 from mail import mail
 
 def main():
+    print("beginning Piccolo")
     spreadsheetID = "1lXvP6G-ZuoAXbisdD2imn-hVQffMbdQWD58rkYnxcIc"
     rangeName = "Assignments!A1:H"
 
     is_weekly_schedule = True
     subject = "It worked"
-    cancelMessage = "Sorry cancelled"
     attendMessage = "Pls"
 
     staffAssignments, startDate, endDate = sheets(spreadsheetID, rangeName)
 
     if is_weekly_schedule:
         if not endDate:
-            print("End Date not specified")
-            return -1
-
-        if after(endDate):
-            print("Passed end date for session: %s" % endDate)
-            return -1
+            print("End Date not specified, program will run until manually stopped.")
+        else:
+            if after(endDate):
+                print("Passed end date for session: %s" % endDate)
+                return -1
 
         activeTimes = scrape.active_shifts()
         activeTimesAsString = list()
@@ -34,19 +33,19 @@ def main():
         for activeTimeString in activeTimesAsString:
             for assignment in staffAssignments:
                 if activeTimeString == assignment[TIME]:
+                    print("%s has active shift at %s" % (assignment[EMAILS], activeTimeString))
                     recipients.update(assignment[EMAILS])
 
         recipients = ', '.join(list(recipients))
         mail(recipients, "PiE", attendMessage, subject)
 
-
     else:
         pass
-    print("finished")
+    print("finishing Piccolo")
 
 
 def after(endDate):
-    return False
+    return endDate > datetime.datetime.today()
 
 if __name__ == "__main__":
     main()
